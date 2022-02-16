39223cd859051b5cb603c33c86267a23b20096ee
neomasterr <neomasterr@mail.ru>
2022-02-16 12:28:23 +0400

    master Event emitter mixin

--- event-emitter.js
+++ event-emitter.js
@@ -4,94 +4,94 @@
 * https://github.com/neomasterr/event-emitter
 */
 function EventEmitter(events = {}) {
-    const _events = {};
-
-    /**
-     * Внутренний метод для добавления событий
-     * @param {String} event   Имя события
-     * @param {Object} options Параметры
-     */
-    const _listen = function(event, options) {
-        const callbacks = [];
-        if (typeof options.callback == 'function') {
-            callbacks.push(options.callback);
-        } else if (typeof options.callback == 'object' && Array.isArray(options.callback)) {
-            options.callback.filter(callback => typeof callback == 'function').forEach(callback => {
-                callbacks.push(callback);
-            });
-        }
-
-        if (!_events[event]) {
-            _events[event] = [];
-        }
-
-        callbacks.forEach(callback => {
-            const data = {
-                once: options.once || false,
-                callback,
-            }
-
-            _events[event].push(data);
-        });
-
-        return _events[event].length;
-    }
+    this.events = {};
 
     if (typeof events == 'object' && events !== null) {
         for (let event in events) {
-            _listen(event, {callback: events[event]});
+            this._listen(event, {callback: events[event]});
         }
     }
+}
 
-    /**
-     * Подписываемся на событие
-     * @param {String}   event    Имя события
-     * @param {Function} callback Вызываемый метод
-     */
-    this.on = function(event, callback) {
-        return _listen(event, {
-            callback,
+/**
+ * Внутренний метод для добавления событий
+ * @param {String} event   Имя события
+ * @param {Object} options Параметры
+ */
+EventEmitter.prototype._listen = function(event, options) {
+    const callbacks = [];
+    if (typeof options.callback == 'function') {
+        callbacks.push(options.callback);
+    } else if (typeof options.callback == 'object' && Array.isArray(options.callback)) {
+        options.callback.filter(callback => typeof callback == 'function').forEach(callback => {
+            callbacks.push(callback);
         });
     }
 
-    /**
-     * Одноразовое событие
-     * @param {String}   event    Имя события
-     * @param {Function} callback Вызываемый метод
-     */
-    this.once = function(event, callback) {
-        return _listen(event, {
-            once: true,
-            callback,
-        });
+    if (!this.events[event]) {
+        this.events[event] = [];
     }
 
-    /**
-     * Вызов события
-     * @param  {String}  event Имя события
-     * @param  {Object}  data  Данные
-     * @return {Boolean}       true если выполнение было прервано, undefined если нет подписчиков
-     */
-    this.emit = function(event) {
-        // нет обработчиков
-        if (!_events[event] || !_events[event].length) {
-            return;
+    callbacks.forEach(callback => {
+        const data = {
+            once: options.once || false,
+            callback,
         }
 
-        // параметры
-        const args = [].slice.call(arguments, 1);
+        this.events[event].push(data);
+    });
 
-        // вызов
-        const interrupted = _events[event].some(item => {
-            // возврат true означает прерывание вызова событий (handled)
-            return item.callback.apply(this, args) === true;
-        });
+    return this.events[event].length;
+}
 
-        // забываем одноразовые события
-        _events[event] = _events[event].filter(item => !item.once);
+/**
+ * Подписываемся на событие
+ * @param {String}   event    Имя события
+ * @param {Function} callback Вызываемый метод
+ */
+EventEmitter.prototype.on = function(event, callback) {
+    return this._listen(event, {
+        callback,
+    });
+}
 
-        return interrupted;
+/**
+ * Одноразовое событие
+ * @param {String}   event    Имя события
+ * @param {Function} callback Вызываемый метод
+ */
+EventEmitter.prototype.once = function(event, callback) {
+    return this._listen(event, {
+        once: true,
+        callback,
+    });
+}
+
+/**
+ * Вызов события
+ * @param  {String}  event Имя события
+ * @param  {Object}  data  Данные
+ * @return {Boolean}       true если выполнение было прервано, undefined если нет подписчиков
+ */
+EventEmitter.prototype.emit = function(event) {
+    // нет обработчиков
+    if (!this.events[event] || !this.events[event].length) {
+        return;
     }
+
+    // параметры
+    const args = [].slice.call(arguments, 1);
+
+    // вызов
+    const interrupted = this.events[event].some(item => {
+        // возврат true означает прерывание вызова событий (handled)
+        return item.callback.apply(this, args) === true;
+    });
+
+    // забываем одноразовые события
+    this.events[event] = this.events[event].filter(item => !item.once);
+
+    return interrupted;
 }
 
 export default EventEmitter;
